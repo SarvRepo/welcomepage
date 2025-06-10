61d9a918fcaf004f6a125338dcee5036bfde26a1
SarvRepo <sinanekoueemail@gmail.com>
2025-06-10 09:56:15 +0330

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-  document.body.classList.add('dark');
-}
-
-function toggleTheme() {
-  const isDark = document.body.classList.toggle('dark');
-  localStorage.setItem('theme', isDark ? 'dark' : 'light');
-}
